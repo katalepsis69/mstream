9a8375631b093b2129fe33b6bfcd0dc75e497568
Carlo Dee <carlodee.official@proton.me>
2025-11-10 23:00:01 +0800

    cleanup css with vendor prefixes.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- mstream/src/main.jsx
+++ mstream/src/main.jsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx'
 import './styles/globals.css'
+import './styles/pages.css'
+import './styles/components.css'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
